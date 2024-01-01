6e561e0f18e15b200bc6d3e7a46026b3d96a8e0c
Aaav <rakesh.g2709.com>
2024-01-01 20:31:41 +0530

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// app.js
+import React from 'react';
+import ProgressBar from './Components/ProgressBar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>React Progress Bar App</h1> */}
+      <ProgressBar />
+    </div>
+  );
+}
+
+export default App;
